4ce97157319ddced4f8045c927d4975d03d77b78
Lucas Matheus <lucasag0408@outlook.com>
2024-07-19 15:17:04 -0400

    mysql configurations

--- config/mysql.js
+++ config/mysql.js
@@ -1,10 +1,11 @@
 const
-  mysql = require('mysql'),
+  mysql = require('mysql2'),
   hl = require('handy-log'),
   { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env
 const db = mysql.createConnection({
   host: MYSQL_HOST,
   user: MYSQL_USER,
+  port: 6033,
   password: MYSQL_PASSWORD,
   database: MYSQL_DATABASE,
   charset: 'utf8mb4'
