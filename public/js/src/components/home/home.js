571736716aab9835413f897757e7e11afe551bda
Lucas Matheus <lucasag0408@outlook.com>
2024-07-19 12:54:31 -0400

    add logic and buton for draws between players!

--- public/js/src/components/home/home.js
+++ public/js/src/components/home/home.js
@@ -5,6 +5,9 @@ import { FadeIn } from 'animate-components'
 import Card from './card'
 import { facemash } from '../../store/actions/facemash-a'
 
+import { post } from 'axios'
+import Notify from 'handy-notification'
+
 @connect(store => {
   return {
     facemash: store.Facemash
@@ -22,6 +25,25 @@ export default class Home extends React.Component {
     this.props.dispatch(facemash())
   }
 
+  vote_tie = async (users) => {
+
+    const user = users[0]
+    const against = users[1]
+    const photo = 'tie'
+
+    const tieVote = {user, against, photo}
+
+    console.log('TIE VOTE DATE =>', tieVote)
+
+    await post('/api/vote', tieVote)
+
+    Notify({ value: 'Tie'})
+
+    this.props.dispatch(facemash())
+
+  }
+
+
   render(){
     let { users } = this.props.facemash
 
@@ -38,9 +60,13 @@ export default class Home extends React.Component {
             </div>
 
             <div className='fm_main'>
-              <Card user={users[0]} against={users[1]} photo='first' />
+              <Card user={users[0]} against={users[1]} photo='winner' />
               <span className='fm_or'>ou</span>
-              <Card user={users[1]} against={users[0]} photo='second' />
+              <Card user={users[1]} against={users[0]} photo='winner' />
+            </div>
+
+            <div className='fm_bottom'>
+              <a href='#' className='refresh pri_btn' onClick={() => this.vote_tie(users)} > Tie </a>
             </div>
 
             <div className='fm_bottom'>
